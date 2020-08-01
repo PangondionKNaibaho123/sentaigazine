3c9b5955d0aa69c83fc1f7eb3ffa8c80ecaed830
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2020-08-01 12:12:42 +0700

    Membuat loading halaman depan

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:sentaigazine/content_container.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,38 +17,68 @@ class MyApp extends StatelessWidget {
   
 }
 
-class ProgressOpenerScreen extends StatelessWidget{
+class ProgressOpenerScreen extends StatefulWidget{
+  ProgressOpenerScreen({Key key}) : super(key : key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return new _ProgressOpenerScreen();
+  }
+
+}
+
+class _ProgressOpenerScreen extends State<ProgressOpenerScreen>{
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
+      body: openingPage(),
       backgroundColor: Colors.lightBlueAccent,
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 220,bottom: 220),
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: 200,
-                width: 250,
-                child: Image.asset("image_source/super_sentai_logo.jpeg"),
-              ),
-              Container(
-                height: 100,
-                width: 200,
-                child: Text(
-                  "SentaiGazine",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
-              ),
-              Container(
-                child: const CircularProgressIndicator(),
-              )
-            ],
-          ),
-        )
-      ),
     );
+  }
+
+  Widget openingPage(){
+      return new Center(
+          child: Container(
+            padding: EdgeInsets.only(top: 220,bottom: 220),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 200,
+                  width: 250,
+                  child: Image.asset("image_source/super_sentai_logo.jpeg"),
+                ),
+                Container(
+                  height: 100,
+                  width: 200,
+                  child: Text(
+                    "SentaiGazine",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white, fontSize: 30),
+                  ),
+                ),
+                Container(
+                  child: const CircularProgressIndicator(),
+                )
+              ],
+            ),
+          )
+      );
+  }
+
+  _loadData() async{
+    var duration = new Duration(seconds: 5);
+    return new Timer(duration, route);
+  }
+
+  route(){
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => ContainerScreen()));
   }
 
 }
